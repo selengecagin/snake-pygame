4751d875da0c314fea7837873fe6b31ecf79538c
selengecagin <selengecagin@gmail.com>
2024-08-26 16:46:05 +0300

    Track and plot scores

--- agent.py
+++ agent.py
@@ -131,9 +131,13 @@ def train():
                 record = score
                 agent.model.save()
 
-            print('Game',agent.n_games,'Score',score,'Record:',record)
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
-            # TODO: plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 
 if __name__ == '__main__'
