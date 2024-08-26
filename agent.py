b7a6939f5bfded8abd50c71cd4b71e1cec3d74bb
selengecagin <selengecagin@gmail.com>
2024-08-26 14:35:52 +0300

    Add placeholder methods in agent class

--- agent.py
+++ agent.py
@@ -11,3 +11,15 @@ LR = 0.001
 class Agent:
     def __init__(self):
         pass
+    def get_state(self,game):
+        pass
+    def remember(self,state,action,reward,next_state,done):
+        pass
+    def train_long_memory(self):
+        pass
+    def train_short_memory(self):
+        pass
+    def get_action(self,state):
+        pass
+    
+    
