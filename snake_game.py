6c2194f3b6c9207e640af41a6e7a9b10e6bb5121
selengecagin <selengecagin@gmail.com>
2024-08-26 14:12:18 +0300

    Remove main game loop due to elimination of user input requirement

--- snake_game.py
+++ snake_game.py
@@ -154,18 +154,3 @@ class SnakeGameAI:
             
         self.head = Point(x, y)
             
-
-if __name__ == '__main__':
-    game = SnakeGame()
-    
-    # game loop
-    while True:
-        game_over, score = game.play_step()
-        
-        if game_over == True:
-            break
-        
-    print('Final Score', score)
-        
-        
-    pygame.quit()
