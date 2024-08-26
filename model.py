51c9ab723131a2f4f697e4ac6f93a927a53e0148
selengecagin <selengecagin@gmail.com>
2024-08-26 16:40:54 +0300

    Implement Q value update logic based on reward and future predictions

--- model.py
+++ model.py
@@ -48,3 +48,7 @@ class QTrainer:
 
             # 1: predicted Q values with current state
             pred = self.model(state)
+
+            # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+            # pred.clone()
+            # preds[argmax(action)] = Q_new
