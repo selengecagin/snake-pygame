c27904ba771c26bbd8646b6853c4034dc491dd81
selengecagin <selengecagin@gmail.com>
2024-08-26 16:16:26 +0300

    Complete model saving with file path

--- model.py
+++ model.py
@@ -19,3 +19,6 @@ class Linear_QNet(nn.Module):
         model_folder_path = './model'
         if not os.path.exists(model_folder_path):
         os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
