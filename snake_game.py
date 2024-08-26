651615de878ec2fbaf222b8ce2068972d03f2806
selengecagin <selengecagin@gmail.com>
2024-08-26 04:09:32 +0300

    Define Direction enum and Point namedtuple for coordinate handling

--- snake_game.py
+++ snake_game.py
@@ -6,3 +6,11 @@ from collections import namedtuple
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
 #font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+ 
+Point = namedtuple('Point', 'x, y')
