736743e81ea31815c7e22a397dcd158780f674c1
selengecagin <selengecagin@gmail.com>
2024-08-26 16:11:56 +0300

    Define Linear_QNet class

--- model.py
+++ model.py
@@ -3,3 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+
+class Linear_QNet(nn.Module):
+        def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
