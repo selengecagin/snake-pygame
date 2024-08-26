7ebad73573bf4576841502a122b267b97948bdd1
selengecagin <selengecagin@gmail.com>
2024-08-26 04:14:45 +0300

    Add collision check and recursive call to avoid placing food on snake

--- snake_game.py
+++ snake_game.py
@@ -52,3 +52,6 @@ class SnakeGame:
        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
 
        self.food = Point(x, y)
+
+         if self.food in self.snake:
+        self._place_food()
