174219b47730f03806ba1e4eac9851f810676b30
selengecagin <selengecagin@gmail.com>
2024-08-26 04:10:07 +0300

    Define RGB color constants and game settings

--- snake_game.py
+++ snake_game.py
@@ -14,3 +14,13 @@ class Direction(Enum):
     DOWN = 4
  
 Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 20
