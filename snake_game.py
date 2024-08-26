955d6086a168274b99f2d00d7832c92c9e288ae9
selengecagin <selengecagin@gmail.com>
2024-08-26 14:09:21 +0300

    Implement left turn direction logic

--- snake_game.py
+++ snake_game.py
@@ -132,9 +132,13 @@ class SnakeGameAI:
         if np.array_equal(action,[1,0,0]):
             new_dir = clock_wise[idx] # no change
     
-        if np.array_equal(action,[0,1,0]):
+        elif np.array_equal(action,[0,1,0]):
             next_idx = (idx +1) % 4
-            new_dir = clock_wise[idx] # right turn r -> d -> l -> u
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0,0,1]
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
 
 
         x = self.head.x
