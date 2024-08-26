03626d0864ff642457c265763064b63bcc1faeac
selengecagin <selengecagin@gmail.com>
2024-08-26 16:14:09 +0300

    Implement model saving functionality with directory creation

--- model.py
+++ model.py
@@ -14,3 +14,8 @@ class Linear_QNet(nn.Module):
         x = F.relu(self.linear1(x))
         x = self.linear2(x)
         return x
+
+  def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+        os.makedirs(model_folder_path)
