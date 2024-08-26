956af1c0c7bf80022fc0e0da7b5cc5f6e43d939f
selengecagin <selengecagin@gmail.com>
2024-08-26 16:17:28 +0300

    Define QTrainer class with optimizer and loss function

--- model.py
+++ model.py
@@ -22,3 +22,11 @@ class Linear_QNet(nn.Module):
 
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
