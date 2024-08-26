434eb63c6d24d58dd0d3ca0d7d19ebb685428523
selengecagin <selengecagin@gmail.com>
2024-08-26 16:06:10 +0300

    Refine epsilon-greedy logic for randomized and model-based action selection

--- agent.py
+++ agent.py
@@ -84,6 +84,16 @@ class Agent:
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
         final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
 
     
 def train():
