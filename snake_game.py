c22b35b0c7b6827eb6a89b0c618478e583ff70e2
selengecagin <selengecagin@gmail.com>
2024-08-26 04:15:19 +0300

    Fix typos

--- snake_game.py
+++ snake_game.py
@@ -48,10 +48,9 @@ class SnakeGame:
         self._place_food()
 
     def _place_food(self):
-       x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
-       y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-
-       self.food = Point(x, y)
-
-         if self.food in self.snake:
-        self._place_food()
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+    
